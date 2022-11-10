73b99f56e81145572dfb26f6ba4983011c82a5a8
Victória Rodrigues <86027764+ToriRodrigues@users.noreply.github.com>
2022-11-09 22:26:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const nome = "Victória"; //constante
+var variavel = 0; //publico
+let v2 = 0; //privado a este html
+if(v2 > 6){
+    alert("Aprovado");
+}else{
+    alert("Reprovado");
+}
+for(var i = 0; i < 1; i++){
+    alert(i);
+}
+
+console.log(document.getElementById("quadro"))
+function bgPink();{
+    document.getElementById("quadro"). 
+    style.height="500px";}
+function bgPurple();{
+    document.getElementById("quadro"). 
+    style.height="500px";}
+function bgBlack();{
+    document.getElementById("quadro"). 
+    style.backgroundColor="#00000";
+}
+   
